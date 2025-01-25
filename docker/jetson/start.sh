0bb2a8ab4e71c981d82dac0ae0f49f0889f18cd5
Nick Ferguson <ndferguson@protonmail.com>
2025-01-25 15:46:40 -0500

    docker attmpt 4

--- docker/jetson/start.sh
+++ docker/jetson/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo docker build -t jetson_bridge .
-sudo docker run -d --rm -it --runtime nvidia --network host --gpus all --device=/dev/ttyACM0 -e DISPLAY --name pitt_urc_jetson jetson_bridge
+sudo docker build -t urc_jetson .
+sudo docker run -d --rm -it --runtime nvidia --network host --gpus all --device=/dev/ttyACM0 -e DISPLAY --name pitt_urc_jetson urc_jetson
