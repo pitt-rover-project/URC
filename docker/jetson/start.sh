fbc982b979a43c5cf748b50d6859a9030b938df7
Nick Ferguson <ndferguson@protonmail.com>
2025-01-25 16:27:27 -0500

    docker compose

--- docker/jetson/start.sh
+++ docker/jetson/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sudo docker build -t urc_jetson .
-sudo docker run -d --rm -it --runtime nvidia --network host --gpus all --device=/dev/ttyACM0 -e DISPLAY --name pitt_urc_jetson urc_jetson
+sudo docker-compose up -d
