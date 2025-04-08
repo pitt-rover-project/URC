8c29462d8f266bdb7f1b23fbf305fb8f91614070
sandoval-williamj <sandoval.williamj@gmail.com>
2025-04-07 20:12:11 -0400

    Working on integrating GPS subscriber into the program. Fixing problems in arudino bridge base of missing motor code.

--- guis/subscribers/Satellites.py
+++ guis/subscribers/Satellites.py
@@ -6,7 +6,7 @@ class SNR:
         fields = nmea_sentence.split(',')
         
         # Check if it's a valid GAGSV message
-        if not fields[0].startswith("GAGSV") or fields[0].startswith("GBGSV"):
+        if not fields[0].startswith("GAGSV") or not fields[0].startswith("GBGSV"):
             return None  # Not a GAGSV or GBGSV sentence
         
         snr_values = []  # Array to store extracted SNR values
@@ -20,19 +20,19 @@ class SNR:
         
         return snr_values
     
-    # Example
-    gagsv_sentence = "GAGSV,2,1,05,02,74,123,46,25,21,129,39,30,48,312,24,34,56,204,26,278"
-    snr_list = parse_gagsv(gagsv_sentence)
-    print(snr_list)  # Output: [46, 39, 24, 26]
+    # # Example
+    # gagsv_sentence = "GAGSV,2,1,05,02,74,123,46,25,21,129,39,30,48,312,24,34,56,204,26,278"
+    # snr_list = parse_gagsv(gagsv_sentence)
+    # print(snr_list)  # Output: [46, 39, 24, 26]
 
-    sum = 0
-    count = 0
-    for i in snr_list: # Compute average SNR, higher is better
-        sum += i
-        count += 1
+    # sum = 0
+    # count = 0
+    # for i in snr_list: # Compute average SNR, higher is better
+    #     sum += i
+    #     count += 1
 
-    average = sum / count
-    print ("Average SNR: ", average) 
+    # average = sum / count
+    # print ("Average SNR: ", average)
 
 class Coordinates:
     def parse_gngll(nmea_sentence):
@@ -52,7 +52,7 @@ class Coordinates:
 
         return 'Latitude: ' + lat + ' ' + lat_dir + ' Longitude: ' + lon + ' ' + lon_dir
 
-    # Example
-    gngll_sentence = "GNGLL,4026.62593,N,07957.49971,W,210345.00,A,A*61"
-    coordinates = parse_gngll(gngll_sentence)
-    print (coordinates)
+    # # Example
+    # gngll_sentence = "GNGLL,4026.62593,N,07957.49971,W,210345.00,A,A*61"
+    # coordinates = parse_gngll(gngll_sentence)
+    # print (coordinates)
