a05af4eb963b4ca8e40370d2aac103c65156923a
Mia Falbo <mlf112@pitt.edu>
2025-06-27 19:21:30 -0400

    moved imu code and updated motor code

--- motor_serial/motor_serial.ino
+++ motor_serial/motor_serial.ino
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #define PWM1Front 3 //all 1's are on right side of robot
 #define PWM1Middle 5
 #define PWM1Back 6
@@ -11,7 +12,10 @@
 #define DIR2Middle 51
 #define DIR2Back 49
 
-int pwmSpeed = 0;
+// REPLACE
+#define WHEEL_BASE 0.7384    // distance between the left and right wheels in meters
+#define MAX_LINEAR_SPEED 1.0    // maximum linear speed in m/s
+#define MAX_PWM 255
 
 void setup() 
 {
@@ -30,10 +34,9 @@ void setup()
   pinMode(PWM2Front,   OUTPUT);
   pinMode(PWM2Middle,   OUTPUT);
   pinMode(PWM2Back,   OUTPUT);
-//
-//  pwmSpeed = Serial.parseInt();
 }
 
+/*
 void forward() {
   digitalWrite(DIR1Front, HIGH);
   digitalWrite(DIR1Middle, HIGH);
@@ -156,15 +159,130 @@ void stopAll() {
   analogWrite(PWM2Front, 0);
   analogWrite(PWM2Middle, 0);
   analogWrite(PWM2Back, 0);
+}*/
+/*
+float min(float x, float y) {
+  return (x > y) ? y : x;
 }
+*/
+
+
 
 void loop() {
-  
+  if (Serial.available()) {
+    String data = Serial.readStringUntil('\n');
+
+    float linear_x, linear_y, linear_z;
+    float angular_x, angular_y, angular_z;
+    
+    /*
+    int parsed = sscanf(data.c_str(), "%f,%f,%f,%f,%f,%f",
+                        &linear_x, &linear_y, &linear_z,
+                        &angular_x, &angular_y, &angular_z);
+    */
+
+    int end = data.indexOf(',');
+    String newData = data;
+    int parsed = 0;
+    Serial.println(parsed);
+
+    while(end != -1) {
+      parsed++;
+      Serial.print("parsed: ");
+      Serial.println(parsed);
+      String sub = newData.substring(0, end);
+      Serial.println(sub);
+
+      if(parsed == 1) {
+        linear_x = sub.toFloat();
+        Serial.println(linear_x);
+      } else if(parsed == 2) {
+        linear_y = sub.toFloat();
+        Serial.println(linear_y);
+      } else if(parsed == 3) {
+        linear_z = sub.toFloat();
+        Serial.println(linear_z);
+      } else if(parsed == 4) {
+        angular_x = sub.toFloat();
+        Serial.println(angular_x);
+      } else if(parsed == 5) {
+        angular_y = sub.toFloat();
+        Serial.println(angular_y);
+      }
+
+      newData = newData.substring(end+1);
+      end = newData.indexOf(',');
+
+      if(end == -1) {
+        angular_z = newData.toFloat();
+        Serial.println(angular_z);
+        parsed++;
+      }
+    }
+
+
+    Serial.println(linear_x);
+    Serial.println(linear_y);
+    Serial.println(linear_z);
+    Serial.println(angular_x);
+    Serial.println(angular_y);
+    Serial.println(angular_z);
+    Serial.println(parsed);
+    
+    if (parsed == 6) {
+      Serial.print("Received: ");
+      Serial.print("Linear (");
+      Serial.print(linear_x); Serial.print(", ");
+      Serial.print(linear_y); Serial.print(", ");
+      Serial.print(linear_z); Serial.print(") | ");
+      Serial.print("Angular (");
+      Serial.print(angular_x); Serial.print(", ");
+      Serial.print(angular_y); Serial.print(", ");
+      Serial.print(angular_z); Serial.println(")");
+
+      float left_speed = linear_x - (angular_z * (WHEEL_BASE / 2.0));
+      float right_speed = linear_x + (angular_z * (WHEEL_BASE / 2.0));
+
+      int pwm_left  = (int)(min(abs(left_speed) / MAX_LINEAR_SPEED, 1.0) * MAX_PWM);
+      int pwm_right = (int)(min(abs(right_speed) / MAX_LINEAR_SPEED, 1.0) * MAX_PWM);
+
+      if (left_speed >= 0) {
+        digitalWrite(DIR2Front, HIGH);
+        digitalWrite(DIR2Middle, HIGH);
+        digitalWrite(DIR2Back, HIGH);
+      } else {
+        digitalWrite(DIR2Front, LOW);
+        digitalWrite(DIR2Middle, LOW);
+        digitalWrite(DIR2Back, LOW);
+      }
+      
+      // Set directions based on the sign of the speed for the right side motors
+      if (right_speed >= 0) {
+        digitalWrite(DIR1Front, HIGH);
+        digitalWrite(DIR1Middle, HIGH);
+        digitalWrite(DIR1Back, HIGH);
+      } else {
+        digitalWrite(DIR1Front, LOW);
+        digitalWrite(DIR1Middle, LOW);
+        digitalWrite(DIR1Back, LOW);
+      }
+      
+      // Apply the PWM values to the motors
+      analogWrite(PWM2Front, pwm_left);
+      analogWrite(PWM2Middle, pwm_left);
+      analogWrite(PWM2Back, pwm_left);
+      analogWrite(PWM1Front, pwm_right);
+      analogWrite(PWM1Middle, pwm_right);
+      analogWrite(PWM1Back, pwm_right);
+    }
+  }
+
+  /*
   String input = Serial.readString();
   input.trim();
   if (!(input.equals("")))
   {
-    Serial.print("input change detected, input is: ");
+    Serial.print("Input change detected, input is: ");
     Serial.println(input);
     Serial.println(input.equals("1"));
     if (input.equals("1"))
@@ -188,12 +306,14 @@ void loop() {
       Serial.println("STOP!");
       stopAll();
     }
-    else {
+    else if(input.equals("7")) {
       int changeSpeed = input.toInt();
       Serial.println("Boost!");
       pwmSpeed = boost(pwmSpeed, changeSpeed);
     }
-  }
+  }*/
+  
   Serial.flush();
-  input = "0";
+  //input = "0";
+
 }
