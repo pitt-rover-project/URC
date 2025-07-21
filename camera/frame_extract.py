7d6d1c2a64a6d1eb5e0e2eedfa3500eb4ce4dff3
Parth Patel <parthbp24@gmail.com>
2025-07-21 19:26:06 -0400

    Added frame_extract and changed frame_extract

--- camera/frame_extract.py
+++ camera/frame_extract.py
@@ -21,10 +21,11 @@ class RealSenseRGBPublisher(Node):
 
     def __init__(self):
         super().__init__('realsense_rgb_publisher')
+        # Topic, outgoing messages 10 before they are dropped
         self.publisher_ = self.create_publisher(Image, 'camera/color/image_raw', 10)
         self.bridge = CvBridge()
 
-        # RealSense pipeline
+        # RealSense pipeline, capture the image on this object
         self.pipeline = rs.pipeline()
         config = rs.config()
         config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
@@ -35,13 +36,16 @@ class RealSenseRGBPublisher(Node):
         self.timer = self.create_timer(timer_period, self.timer_callback)
 
     def timer_callback(self):
+        # Grab a frame
         frames = self.pipeline.wait_for_frames()
         color_frame = frames.get_color_frame()
         if not color_frame:
             self.get_logger().warn("No color frame received.")
             return
 
+        # publish it
         color_image = np.asanyarray(color_frame.get_data())
+        # rgb_image = cv2.cvtColor(color_image, cv2.COLOR_BGR2RGB) if we need to convert to rgb (uncomment this code)
         msg = self.bridge.cv2_to_imgmsg(color_image, encoding='bgr8')
         self.publisher_.publish(msg)
 
